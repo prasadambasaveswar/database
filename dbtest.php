f9b03dce2def65e0668238384c4c354f98f1ded8
prasadambasaveswar <basaveswar01@gmail.com>
2018-04-23 19:13:50 +0530

    Update dbtest.php

--- dbtest.php
+++ dbtest.php
@@ -4,12 +4,6 @@ $username = getenv("databaseuser");
 $password = getenv("databasepassword");
 $dbname = getenv("databasename");
 
-echo getenv("MYSQL_SERVICE_HOST");
-echo getenv("databaseuser");
-echo getenv("databasepassword");
-echo getenv("databasename");
-
-
 
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
@@ -17,26 +11,7 @@ $conn = new mysqli($servername, $username, $password, $dbname);
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 } 
-
-
 $sql = "INSERT INTO MyTeam (firstname, lastname, email) VALUES ('$_POST[fname]','$_POST[lname]','$_POST[email]')";
-
-echo "===================";
-echo $_POST["fname"];
-echo $_POST["lname"];
-echo $_POST["email"];
-
-echo "===================";
-
-
-//"INSERT INTO nametable (fname, lname)
-
-//VALUES
-
-//('$_POST[fname]','$_POST[lname]')";
-
-
-
 if ($conn->query($sql) === TRUE) {
     echo "New record created successfully";
 	$result = mysqli_query($conn,"SELECT firstname, lastname, email FROM MyTeam;");
@@ -57,44 +32,9 @@ echo "</tr>";
 }
 echo "</table>";
 
-	//$sql2 = "SELECT * FROM MyTeam;";
-//	echo $conn->query($sql2);
-/*	$db = new database();
-	$query = "select * from data";
-        $result = $db->select($query);
-
-        echo "<table>";
-         echo "<tr>";
-            echo "<th>Name </th>";
-            echo "<th>L-Name </th>";
-	echo "<th>Mail </th>";
-         echo "</tr>";
-         while($row = mysqli_fetch_array($result)) 
-         {
-            echo "<tr>";
-            echo "<td> $row[firstname]</td>";
-            echo "<td> $row[lastname]</td>";
-		 echo "<td> $row[email]</td>";
-            echo "</tr>";
-         }
-       echo "</table>";
-    */
 	} else {
     echo "Error: " . $sql . "<br>" . $conn->error;
 }
-
-
-
-
-/*$sql2 = "SELECT * FROM MyTeam;";
-$result = $conn->query($sql2);
-Welcome <?php echo $_GET["name"]; ?><br>
-if ($conn->query($sql2) === TRUE) {
-   echo $result;
-	} else {
-    echo "Error: " . $sql2 . "<br>" . $conn->error;
-}
-*/
 $conn->close();
 ?>
 
