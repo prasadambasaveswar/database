063442e029090fa8681f1489ff4d4702425874ef
prasadambasaveswar <basaveswar01@gmail.com>
2018-04-23 14:57:57 +0530

    Update dbtest.php

--- dbtest.php
+++ dbtest.php
@@ -14,8 +14,8 @@ if ($connection->connect_errno) {
 
 
 
-// sql to create table.
-$sql = "CREATE TABLE MyGuests (
+// sql to create table
+$sql = "CREATE TABLE $_POST["name"] (
 id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY, 
 firstname VARCHAR(30) NOT NULL,
 lastname VARCHAR(30) NOT NULL,
@@ -24,31 +24,9 @@ reg_date TIMESTAMP
 )";
 
 if ($connection->query($sql) === TRUE) {
-    echo "Table MyGuests created successfully\n";
+    echo "Table $_POST["name"] created successfully";
 } else {
     echo "Error creating table: " . $connection->error;
-}
-
-
-$sql = "INSERT INTO MyGuests (firstname, lastname, email)
-VALUES ('bashu', 'pr', 'bprasadam@osius.com')";
-
-if ($connection->query($sql) === TRUE) {
-    echo "New record created successfully \n";
-} else {
-    echo "Error: " . $sql . "<br>" . $connection->error;
-}
-
-$sql = "SELECT id, firstname, lastname FROM MyGuests";
-$result = $connection->query($sql);
-
-if ($result->num_rows > 0) {
-    // output data of each row
-    while($row = $result->fetch_assoc()) {
-        echo "id: " . $row["id"]. " - Name: " . $row["firstname"]. " " . $row["lastname"]. "<br>";
-    }
-} else {
-    echo "0 results";
 }
 
 $connection->close();
