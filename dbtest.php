785c172516dd12f8cac42dfe0b1f0fe2d0c0077f
prasadambasaveswar <basaveswar01@gmail.com>
2018-04-23 15:25:11 +0530

    Update dbtest.php

--- dbtest.php
+++ dbtest.php
@@ -9,13 +9,12 @@ if ($connection->connect_errno) {
     printf("Connect failed: %s\n", $mysqli->connect_error);
     exit();
 } else {
-    printf("demonstartion for inter pod communication \n");
+    printf("demonstartion for inter pod communication");
 }
 
 
-
 // sql to create table
-$sql = "CREATE TABLE $_POST["name"] (
+$sql = "CREATE TABLE MyTeam (
 id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY, 
 firstname VARCHAR(30) NOT NULL,
 lastname VARCHAR(30) NOT NULL,
@@ -24,10 +23,33 @@ reg_date TIMESTAMP
 )";
 
 if ($connection->query($sql) === TRUE) {
-    echo "Table $_POST["name"] created successfully";
+    echo "Table MyTeam created successfully";
 } else {
     echo "Error creating table: " . $connection->error;
 }
+
+
+$sql = "INSERT INTO MyTeam (firstname, lastname, email)
+VALUES ('$_GET["lname"]', '$_GET["name"]', '$_GET["email"]')";
+
+if ($connection->query($sql) === TRUE) {
+    echo "New record created successfully";
+} else {
+    echo "Error: " . $sql . "<br>" . $connection->error;
+}
+
+$sql = "SELECT id, firstname, lastname FROM MyTeam";
+$result = $connection->query($sql);
+
+if ($result->num_rows > 0) {
+    // output data of each row
+    while($row = $result->fetch_assoc()) {
+        echo "id: " . $row["id"]. " - Name: " . $row["firstname"]. " " . $row["lastname"]. "<br>";
+    }
+} else {
+    echo "0 results";
+}
+
 
 $connection->close();
 ?>
