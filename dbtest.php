2554a3d28a4be89e2ccafdad5f9fa90b3c03a4c9
prasadambasaveswar <basaveswar01@gmail.com>
2018-04-23 17:58:15 +0530

    Update dbtest.php

--- dbtest.php
+++ dbtest.php
@@ -4,6 +4,11 @@ $username = getenv("databaseuser");
 $password = getenv("databasepassword");
 $dbname = getenv("databasename");
 
+echo getenv("MYSQL_SERVICE_HOST");
+echo getenv("databaseuser");
+echo getenv("databasepassword");
+echo getenv("databasename");
+
 
 
 // Create connection
@@ -19,30 +24,32 @@ VALUES ('".$_get['fname']."', '".$_get['lname']."', '".$_get['email']."')";
 
 if ($conn->query($sql) === TRUE) {
     echo "New record created successfully";
-	$result = mysqli_query($conn,"SELECT firstname, lastname, email FROM MyTeam;");
-	echo "<table border='1'>
-<tr>
-<th>Firstname</th>
-<th>Lastname</th>
-<th>Email</th>
-</tr>";
+	//$sql2 = "SELECT * FROM MyTeam;";
+//	echo $conn->query($sql2);
+	$db = new database();
+	$query = "select * from data";
+        $result = $db->select($query);
 
-while($row = mysqli_fetch_array($result))
-{
-	echo "-------" . $row;
-echo "<tr>";
-echo "<td>" . $row['firstname'] . "</td>";
-echo "<td>" . $row['lastname'] . "</td>";
-echo "<td>" . $row['email'] . "</td>";
-echo "</tr>";
-}
-echo "</table>";
-	
-//$sql2 = "SELECT * FROM MyTeam;";
-//	echo $conn->query($sql2);	
+        echo "<table>";
+         echo "<tr>";
+            echo "<th>Name </th>";
+            echo "<th>L-Name </th>";
+	echo "<th>Mail </th>";
+         echo "</tr>";
+         while($row = mysqli_fetch_array($result)) 
+         {
+            echo "<tr>";
+            echo "<td> $row[firstname]</td>";
+            echo "<td> $row[lastname]</td>";
+		 echo "<td> $row[email]</td>";
+            echo "</tr>";
+         }
+       echo "</table>";
 	} else {
     echo "Error: " . $sql . "<br>" . $conn->error;
 }
+
+
 
 
 /*$sql2 = "SELECT * FROM MyTeam;";
